33aa36fd21cdbb013e0162898d010382383bd0ff
Cristian Botella <cristian@digio.es>
2017-12-05 09:14:35 +0100

    First commit

---
+++ imports/ui/Task.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+import { Meteor } from 'meteor/meteor';
+import classnames from 'classnames';
+import { Tasks } from '../api/tasks.js';
+
+// Task component - represents a single todo item
+
+export default class Task extends Component {
+  toggleChecked() {
+    /* Tasks.update(this.props.task._id, { // selector that identifies a subset of the collection
+      $set: { checked: !this.props.task.checked }, // what should be done to the matched objects
+    }); */
+    Meteor.call('tasks.setChecked', this.props.task._id, !this.props.task.checked);
+  }
+
+  deleteThisTask() {
+    /* Tasks.remove(this.props.task._id); */
+    Meteor.call('tasks.remove', this.props.task._id);
+  }
+
+  togglePrivate() {
+    Meteor.call('tasks.setPrivate', this.props.task._id, ! this.props.task.private)
+  }
+
+  render() {
+    // Give tasks a different className when they are checked off,
+    // so that we can style them nicely in CSS
+    const taskClassName = classnames({
+      checked: this.props.task.checked,
+      private: this.props.task.private,
+    });
+
+    return (
+      <li className={taskClassName}>
+        <button className="delete" onClick={this.deleteThisTask.bind(this)}>      
+          &times;      
+        </button>
+
+        <input     
+          type="checkbox"
+          readOnly
+          checked={!!this.props.task.checked}
+          onClick={this.toggleChecked.bind(this)}
+        />
+
+        { this.props.showPrivateButton ? (
+          <button className="toggle-private" onClick={this.togglePrivate.bind(this)}>
+            { this.props.tas.private ? 'Private' : 'Public' }
+          </button>
+        ) : ''}
+
+        <span className="text">
+          <strong>{this.props.task.username}</strong>: {this.props.task.text}
+        </span>
+
+      </li>
+    );
+  }
+}
+
